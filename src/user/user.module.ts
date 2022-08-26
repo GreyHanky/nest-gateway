635cedb29fc13923f313de2f714c68d03f4cc4ef
songjiang.liu <songjiang.liu@amh-group.com>
2022-08-26 16:42:22 +0800

    feat: 添加数据库接入

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,15 @@
-import { CacheModule, forwardRef, Module } from '@nestjs/common';
-import { FeishuService } from './feishu/feishu.service';
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '@/common/database/database.module';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { UserProviders } from './user.providers';
 import { FeishuController } from './feishu/feishu.controller';
+import { FeishuService } from './feishu/feishu.service';
 
 @Module({
-  controllers: [FeishuController],
-  providers: [FeishuService],
+  imports: [DatabaseModule],
+  controllers: [FeishuController, UserController],
+  providers: [...UserProviders, UserService, FeishuService],
+  exports: [UserService],
 })
 export class UserModule {}
