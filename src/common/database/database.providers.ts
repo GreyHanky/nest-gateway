f675cf13459459ac9096508af689f45c9410f263
565 <development565@163.com>
2022-09-26 23:36:21 +0800

    feat: 添加mysql数据库连接

--- src/common/database/database.providers.ts
+++ src/common/database/database.providers.ts
@@ -1,15 +1,14 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { getConfig } from 'src/utils/index';
 import * as path from 'path';
+import { UserRole } from '@/userCenter/user-role/entities/user-role.mysql.entity';
 
 // 设置数据库类型
-const databaseType: DataSourceOptions['type'] = 'mongodb';
-const { MONGODB_CONFIG } = getConfig();
+const { MONGODB_CONFIG, MYSQL_CONFIG } = getConfig();
 
 const MONGODB_DATABASE_CONFIG = {
   ...MONGODB_CONFIG,
-  type: databaseType,
-  entities: [   
+  entities: [
     path.join(
       __dirname,
       `../../**/*.${MONGODB_CONFIG.entities}.entity{.ts,.js}`,
@@ -17,16 +16,27 @@ const MONGODB_DATABASE_CONFIG = {
   ],
 };
 
+const MYSQL_DATABASE_CONFIG = {
+  ...MYSQL_CONFIG,
+  entities: [UserRole]
+}
+
 const MONGODB_DATA_SOURCE = new DataSource(MONGODB_DATABASE_CONFIG);
+const MYSQL_DATA_SOURCE = new DataSource(MYSQL_DATABASE_CONFIG);
 
 // 数据库注入
 export const DatabaseProviders = [
   {
     provide: 'MONGODB_DATA_SOURCE',
     useFactory: async () => {
-      await MONGODB_DATA_SOURCE.initialize();
+      if (!MONGODB_DATA_SOURCE.isInitialized) await MONGODB_DATA_SOURCE.initialize();
       return MONGODB_DATA_SOURCE;
     },
-  },
+  }, {
+    provide: 'MYSQL_DATA_SOURCE',
+    useFactory: async () => {
+      if (!MYSQL_DATA_SOURCE.isInitialized) await MYSQL_DATA_SOURCE.initialize();
+      return MYSQL_DATA_SOURCE;
+    },
+  }
 ];
-    
