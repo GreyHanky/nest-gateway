03cda45f0469c0298ff090b727dcfa2d1d8d761a
565 <development565@163.com>
2022-10-04 13:36:49 +0800

    feat: 新增分页公共方法

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,4 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { IPaginationOptions, IPaginationMeta } from 'nestjs-typeorm-paginate';
+import { defaultPaginationParams, MAX_PAGE_SIZE } from './constants';
 
 export const PayloadUser = createParamDecorator(
   (data, ctx: ExecutionContext): Payload => {
@@ -6,3 +8,35 @@ export const PayloadUser = createParamDecorator(
     return request.user;
   },
 );
+
+export class CustomPaginationMeta {
+  constructor(
+    public readonly pageSize: number,
+    public readonly totalCounts: number,
+    public readonly totalPages: number,
+    public readonly currentPage: number,
+  ) { }
+}
+
+export const getPaginationOptions = (
+  page: PaginationParams = {
+    currentPage: defaultPaginationParams.currentPage,
+    pageSize: defaultPaginationParams.pageSize,
+  },
+) => {
+  const limit = page.pageSize > MAX_PAGE_SIZE ? MAX_PAGE_SIZE : page.pageSize;
+
+  const options: IPaginationOptions<CustomPaginationMeta> = {
+    page: page.currentPage,
+    limit,
+    metaTransformer: (meta: IPaginationMeta): CustomPaginationMeta => {
+      return new CustomPaginationMeta(
+        meta.itemCount,
+        meta.totalItems,
+        meta.totalPages,
+        meta.currentPage,
+      );
+    },
+  };
+  return options;
+};
